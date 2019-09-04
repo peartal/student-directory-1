461c715f582b8575eb0cb73b2a2cf783d628906a
Marion <marion.vagnerdamicourt@kpmg.co.uk>
2019-09-04 16:13:21 +0100

    shorten code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,7 @@
+students_count = 11
+
+#first, we print the list of students
+
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,9 +15,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
-print "overall, we have "
-print 11
-puts " great students"
+#Finally, we print the total of numbrr of students
+puts "Overall, we have #{students_count} great students"
+#It's important that print() doesn't add a new line chracters
 
 print "line1\nline2\nline3"
