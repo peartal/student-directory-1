575fd1503449379b9a4e7a325a58562b1d9f7120
“Daniel Khan <Daniel.Khan@Vodafone.com>
2019-09-05 09:52:14 +0100

    adding more information about students

--- methods.rb
+++ methods.rb
@@ -2,25 +2,25 @@
 
 #first, we print the list of students
 students = [
- ["Dr. Hannibal Lecter", :november],
- ["Darth Vader", :november],
- ["Nurse Ratched", :november],
-["Michael Corleone", :november],
- ["Alex DeLarge", :november],
- ["The Wicked Witch of the West", :november],
- ["Terminator", :november],
- ["Freddy Krueger", :november],
- ["The Joker", :november],
- ["Marion", :november],
- ["Dan", :november],
- ["Joffrey Baratheon", :november],
- ["Norman Bates", :november],
- ["Terminator", :november]
+ {name: "Dr. Hannibal Lecter", cohort: :november},
+ {name: "Darth Vader", cohort: :november},
+ {name: "Nurse Ratched", cohort: :november},
+ {name: "Michael Corleone", cohort: :november},
+ {name: "Alex DeLarge", cohort: :november},
+ {name: "The Wicked Witch of the West", cohort: :november},
+ {name: "Terminator", cohort: :november},
+ {name: "Freddy Krueger", cohort: :november},
+ {name: "The Joker", cohort: :november},
+ {name: "Marion", cohort: :november},
+ {name: "Dan", cohort: :november},
+ {name: "Joffrey Baratheon", cohort: :november},
+ {name: "Norman Bates", cohort: :november},
+ {name: "Terminator", cohort: :november}
 ]
 
 def print(students)
   students.each do |student|
-    puts "#{student[0]} (#{student[1]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
